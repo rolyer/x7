44a48c1a55912bd1134acb5526e2500f16699b45
simsevenx <cooperation@qq.com>
2019-01-21 17:28:57 +0800

    1.6.4.RELEASE

--- demo/src/main/java/io/xream/x7/demo/controller/XxxController.java
+++ demo/src/main/java/io/xream/x7/demo/controller/XxxController.java
@@ -44,17 +44,25 @@ public class XxxController {
 	public ViewEntity create(){
 
 		Cat cat = new Cat();
-		cat.setId(243);
+		cat.setId(245);
 		cat.setDogId(2);
 
-		this.catRepository.create(cat);
+//		this.catRepository.create(cat);
 
 		Cat cat2 = new Cat();
-		cat2.setId(244);
+		cat2.setId(246);
 		cat2.setDogId(2);
+//
+//		this.catRepository.create(cat2);
 
-		this.catRepository.create(cat2);
-
+		List<Cat> catList = new ArrayList<>();
+		for (int i = 0; i< 500; i++){
+			Cat cat3 = new Cat();
+			cat3.setId(5448 + i);
+			cat3.setDogId(2);
+			catList.add(cat3);
+		}
+		this.catRepository.createBatch(catList);
 //		throw new RuntimeException("-----------------------------> test wawawawa");
 
 		return ViewEntity.ok();
