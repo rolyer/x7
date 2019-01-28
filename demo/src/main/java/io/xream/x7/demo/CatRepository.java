5028656dfb75d266348b2d77cac6209986c252db
simsevenx <cooperation@qq.com>
2019-01-28 21:11:12 +0800

    1.6.6

--- demo/src/main/java/io/xream/x7/demo/CatRepository.java
+++ demo/src/main/java/io/xream/x7/demo/CatRepository.java
@@ -3,9 +3,8 @@ package io.xream.x7.demo;
 import io.xream.x7.demo.bean.Cat;
 import org.springframework.stereotype.Repository;
 import x7.repository.BaseRepository;
-import x7.repository.inner.impl.DefaultRepository;
 
 @Repository
-public class CatRepository extends DefaultRepository<Cat> {
+public interface CatRepository extends BaseRepository<Cat> {
 
 }
--- demo/src/main/java/io/xream/x7/demo/CatTestRepository.java
+++ demo/src/main/java/io/xream/x7/demo/CatTestRepository.java
@@ -2,11 +2,9 @@ package io.xream.x7.demo;
 
 import io.xream.x7.demo.bean.CatTest;
 import org.springframework.stereotype.Repository;
-
 import x7.repository.BaseRepository;
-import x7.repository.inner.impl.DefaultRepository;
 
 @Repository
-public class CatTestRepository extends DefaultRepository<CatTest> {
+public interface CatTestRepository extends BaseRepository<CatTest> {
 
 }
