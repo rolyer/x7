b0f1e61fd6327aa6669e2bbca4284c206397e931
simsevenx <cooperation@qq.com>
2019-03-15 10:16:42 +0800

    2.0.6.RELEASE

--- demo/src/main/java/io/xream/x7/App.java
+++ demo/src/main/java/io/xream/x7/App.java
@@ -3,6 +3,7 @@ package io.xream.x7;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import x7.EnableDateToLongForJackson;
 import x7.EnableTransactionManagementReadable;
 import x7.EnableX7L2Cache;
 import x7.EnableX7Repository;
@@ -18,6 +19,7 @@ import x7.EnableX7Repository;
 @EnableX7L2Cache(timeSeconds = 120)
 @EnableX7Repository(mappingPrefix = "t_",mappingSpec = "_")
 @EnableReyClient
+@EnableDateToLongForJackson
 public class App {
     public static void main( String[] args )
     {
