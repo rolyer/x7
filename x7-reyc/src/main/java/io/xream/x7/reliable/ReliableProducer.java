11cdabba9fa850925e11cb3c2100b814e3434398
simsevenx <cooperation@qq.com>
2019-11-06 15:32:04 +0800

    2.2.3.RELEASE

--- x7-reyc/src/main/java/io/xream/x7/reliable/ReliableProducer.java
+++ x7-reyc/src/main/java/io/xream/x7/reliable/ReliableProducer.java
@@ -1,7 +1,5 @@
 package io.xream.x7.reliable;
 
-import org.mockito.internal.matchers.Null;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,11 +9,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD})
 public @interface ReliableProducer {
 
-    /**
-     * Tcc implemented to check all resources, if all resouces ok, produce confirm topic
-     * <br>
-     * Anyway, if no deed to check all resources, not value it
-     */
     String topic() default "";
     Class<?> type()  default Void.class;
     String[] svcs() default {};
--- x7-reyc/src/main/java/io/xream/x7/reliable/api/ReliableBackend.java
+++ x7-reyc/src/main/java/io/xream/x7/reliable/api/ReliableBackend.java
@@ -5,13 +5,24 @@ import java.util.concurrent.Callable;
 public interface ReliableBackend {
 
     /**
-     * @Param topicCancel
-     * @Param topicConfirm
-     * as regards to topicCancel & topicConfirm, TCC implemented to check all resources, if all resouces ok, produce topicConfirm
-     * <br>
-     * Anyway, if no deed to check all resources, not value it
+     * @param isTcc
+     * as regards to TCC, TCC implemented to check all resources, <br>
+     *     at first, produce 'TOPIC'_TCC_TRY <br>
+     *     if all resouces ok, produce 'TOPIC'_TCC_CONFIRM <br>
+     *     any exception occured, produce 'TOPIC'_TCC_CANCEL <br>
+     *     anyway, when isTcc = true, has to prepare 3 listeners to listener<br>
+     * @param topic message topic
+     * @param body  message body
+     * @param svcs  the nameList of other listening domain service
+     * @param callable  the service or controller handle the bisiness
      */
     Object produceReliably(Boolean isTcc, String topic, Object body, String[] svcs, Callable callable);
 
+    /**
+     *
+     * @param svc  the name of listening domain service
+     * @param message message body
+     * @param runnable  listener handle the bisiness
+     */
     void onConsumed(String svc, Object message, Runnable runnable);
 }
